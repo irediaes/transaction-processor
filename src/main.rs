80d5dac7a79d95233ac49b8ad4b63ec2b1512fa5
Iredia Ebikade <40021650+irediaes@users.noreply.github.com>
2022-08-06 07:09:16 +0100

    feat: implements reading csv file argument and parsing its content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate csv;
+
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_csv_file() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn read_csv_file() -> Result<(), Box<dyn Error>> {
+    let file_path = read_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn read_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected a csv file argument")),
+        Some(file_path) => Ok(file_path),
+    }
 }
